c0cc5c28d464b71df81898f0fd16a155bbed4f82
nehalkhaled19 <nehalkhaled62@gmail.com>
2023-12-26 01:51:33 +0200

    update trending movies

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './Home.css'
 import { useQuery } from 'react-query';
 import Slider from 'react-slick';
@@ -7,12 +7,10 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { useEffect } from 'react';
 
 export default function Home() {
 
-  // ---------------------Get DATA---------------------//
-
+  // ---------------------First Section---------------------//
   const options = {
     method: 'GET',
     headers: {
@@ -20,17 +18,22 @@ export default function Home() {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlODJjNjg1ZmYyZTcwYWY4Yjc4MmVjNzliODFkMzNhNiIsInN1YiI6IjY0ZmNiZjhiZmZjOWRlMGVkZjVmMDhhNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.djg5chA3Ca-uZ56OqOW4SIyeoQ6cRH3rJAdBk4x1lYQ'
     }
   };
-// fetch data
+  // fetch data
   function getMovies() {
     return fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', options)
       .then(response => response.json())
   }
-// Use React Query
-  let { isLoading, data } = useQuery('Movies', getMovies)
-  let movies = data?.results.slice(15, 20)
 
+  // Use React Query
+  let x = useQuery('Movies', getMovies)
+  let movies = x.data?.results.slice(5, 10)
 
-  // ---------------------slider settings---------------------//
+  // AOS Animation//
+  useEffect(() => {
+    AOS.init();
+  }, [])
+  
+  // First slider settings//
   const settings = {
     dots: false,
     fade: true,
@@ -43,54 +46,107 @@ export default function Home() {
     autoplaySpeed: 3500,
     pauseOnHover: false,
   };
-  // ---------------------AOS animation---------------------//
-  useEffect(() => {
-    AOS.init();
-  }, [])
+  // ---------------------Get Trending Movies---------------------//
+  // fetch data
+  function getTrendingMovies() {
+    fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
+      .then(response => response.json())
+  }
+  // Use React Query
+  let { data } = useQuery('Movies', getMovies)
+  let trendingMovies = data?.results
+  // Sec slider settings//
+  const slider = React.useRef(null);
+  const settings2 = {
+    className: "center",
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 5,
+    swipeToSlide: true,
+    arrows: false,
+    afterChange: function (index) {
+      console.log(
+        `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
+      );
+    }
+  }
 
 
+  return <main >
+    {/* first Section */}ِ
+    <header className='Home mt-5 vh-100 d-flex '>
+      <div className="container d-flex align-items-center">
+        <div className="row  w-100 align-items-center ">
+          {/* ---------------------HeadLine--------------------- */}
+          <div className="col-md-6">
+            <h6 className='main-color main-font-weight' data-aos="fade-up"
+              data-aos-duration="700">Movflx</h6>
+            <h2 className='text-white my-3 main-font-weight' data-aos="fade-up"
+              data-aos-duration="900" data-aos-delay="250">  Unlimited <span className='main-color'>Movie</span>, TV Shows, & More</h2>
+            <button className='btn rounded-5 main-font-weight ' data-aos="fade-up"
+              data-aos-duration="1100" data-aos-delay="500">
+              <i className='fa-solid fa-play me-2 '></i> WATCH NOW
+            </button>
+          </div>
+          {/* ---------------------Slider--------------------- */}
+          <div className="col-md-6  p-0">
+            <Slider  {...settings}   >
+              {movies?.map((e) => (
+                <div data-aos="fade-left" style={{ height: '300px' }} data-aos-duration="1100" data-aos-delay="500" key={e.title} >
+                  <div
+                    className="layer-carrier rounded-3">
+                    <img
+                      src={`https://image.tmdb.org/t/p/original${e.backdrop_path}`}
+                      className='w-100 h-100 rounded-3 image'
+                      alt={e.title}
+                    />
+                    <div className='movie-layer rounded-3'></div>
+                  </div>
+                </div>
+              ))}
 
-  return <section className='Home mt-5 vh-100 d-flex '>
+            </Slider>
 
 
-    <div className="container d-flex align-items-center">
-      <div className="row  w-100 align-items-center ">
-        {/* ---------------------HeadLine--------------------- */}
-        <div className="col-md-6">
-          <h6 className='main-color' data-aos="fade-up"
-            data-aos-duration="500">Movflx</h6>
-          <h2 className='text-white my-3' data-aos="fade-up"
-            data-aos-duration="500" data-aos-delay="500">  Unlimited <span className='main-color'>Movie</span>, TV Shows, & More</h2>
-          <button className='btn rounded-5 ' data-aos="fade-up"
-            data-aos-duration="500" data-aos-delay="1000">
-            <i className='fa-solid fa-play me-2 '></i> WATCH NOW
-          </button>
+          </div>
         </div>
-        {/* ---------------------Slider--------------------- */}
-        <div className="col-md-6  p-0">
-          <Slider  {...settings}   >
-            {movies?.map((e) => (
-              <div data-aos="fade-left" data-aos-duration="700" data-aos-delay="1200" key={e.title} >
-                <div
-                  data-aos-anchor="#example-anchor"
-                  data-aos-offset="1000"
-                  data-aos-duration="500" className="layer-carrier rounded-3">
-                  <img
-                    src={`https://image.tmdb.org/t/p/original${e.backdrop_path}`}
-                    className='w-100 rounded-3 image'
-                    alt={e.title}
-                  />
-                  <div className='movie-layer rounded-3'></div>
+      </div>
+    </header>
+
+    {/* Trending Movies Section */}
+    <section className='trending-movies py-5'>
+      <div className="container">
+        <div className="d-flex ">
+          <h2 className='position-relative main-font-weight'>Trending Movies</h2>
+          {/* Custom arrow container */}
+          <div className="custom-arrows-container ms-auto">
+            <div className='slider-btn rounded-4 '>
+              <button className='btn px-3 position-relative prev' onClick={() => slider?.current?.slickPrev()}>
+                <i class="fas fa-angle-left left"></i>
+              </button>
+              <button className='btn px-3 ' onClick={() => slider?.current?.slickNext()}>
+                <i class="fas fa-angle-right arrow"></i>
+              </button>
+            </div>
+          </div>
+
+        </div>
+        <main className='row flex-wrap gy-4'>
+          <Slider ref={slider} {...settings2} className='px-2'  >
+            {trendingMovies?.map((e) => (
+              <div className='col-md-2 ' key={e.id}>
+                <div className='py-3 px-0 '>
+                  <div className='position-relative mx-3'>
+                    <img src={`https://image.tmdb.org/t/p/original${e.poster_path}`} className='w-100 rounded-3' alt={e.title} />
+                    <h6 className='my-3'>{e.title}</h6>
+                    <div role="progressbar" className='vote' aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" rate={e.vote_average} style={{ "--value": `${e.vote_average}` }}></div>
+                  </div>
                 </div>
               </div>
             ))}
-
           </Slider>
-
-
-        </div>
+        </main>
       </div>
-    </div>
-  </section>
-
+    </section>
+  </main>
 }
